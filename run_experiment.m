6611c747ff146a9385ba60ee0064e867d8e8cbf8
FerminT <fermintravi@gmail.com>
2022-08-23 17:46:23 -0300

    Shuffled stimuli in blocks per subject

--- run_experiment.m
+++ run_experiment.m
@@ -1,16 +1,42 @@
 % Constants
-SAVE_PATH = 'Data';
+SAVE_PATH     = 'Data';
+METADATA_PATH = 'Metadata';
+stimuli_splits = [6 6 5];
 
-[initials, reading_level] = initial_questions();
-if isempty(initials); return; end
+[subjname, reading_level] = initial_questions();
+if isempty(subjname); return; end
 
-SAVE_PATH = fullfile(SAVE_PATH, initials);
+SAVE_PATH = fullfile(SAVE_PATH, subjname);
 if exist(SAVE_PATH, 'dir') ~= 7
     mkdir(SAVE_PATH)
 end
+subjfile = fullfile(SAVE_PATH, 'metadata');
 
+load(fullfile(METADATA_PATH, 'stimuli_config.mat'));
+load(fullfile(METADATA_PATH, 'stimuli_order.mat'));
+% Sanity check
+if length(stimuli_order.title) ~= sum(stimuli_splits)
+    disp('ERROR: la suma de los bloques no condice con la cantidad de textos')
+    return
+end
+
+shuffled_stimuli = shuffle_in_blocks(stimuli_splits, stimuli_order.title);
+stimuli_index = 1;
+
+save(subjfile, 'subjname', 'reading_level', 'shuffled_stimuli', 'stimuli_index')
 
 filename  = fullfile(SAVE_PATH, filenames{idstimuli});
+
+function shuffled_elems = shuffle_in_blocks(blocks_size, elems)
+    shuffled_elems   = {};
+    block_startindex = 1;
+    for split_index = 1:length(blocks_size)
+        block_finishindex = block_startindex + blocks_size(split_index) - 1;
+        current_block     = elems(block_startindex:block_finishindex);
+        shuffled_elems    = cat(1, shuffled_elems, current_block(randperm(blocks_size(split_index))));
+        block_startindex  = block_finishindex + 1;
+    end
+end
 
 function [initials, reading_level] =  initial_questions()
     prompt = {'Ingrese sus iniciales (incluya segundo nombre, si lo tiene):', ...
@@ -22,7 +48,7 @@ function [initials, reading_level] =  initial_questions()
         return
     else
         initials = upper(answer{1});
-        if length(answer) > 1
+        if ~isempty(answer{2})
             reading_level = answer{2};
         else
             reading_level = 'N/C';
