641d690ef0e6d27d37f1539d573e2baeab2489ef
FerminT <fermintravi@gmail.com>
2023-02-09 11:21:53 -0300

    Add validation check when processing data

--- Code/data_processing/extract_fixations.py
+++ Code/data_processing/extract_fixations.py
@@ -42,8 +42,8 @@ def save_profile(participant_path, save_path):
     profile  = {'name': [metafile['subjname']], 'reading_level': [int(metafile['reading_level'])]}
     pd.DataFrame(profile).to_pickle(save_path / 'profile.pkl')
     
-def save_validation_fixations(trial_msgs, trial_fix, trial_path):
-    val_msgs = trial_msgs[trial_msgs['text'].str.contains('validation')]
+def save_validation_fixations(trial_msgs, trial_fix, trial_path, val_legend='validation', num_points=9, points_area=56):
+    val_msgs = trial_msgs[trial_msgs['text'].str.contains(val_legend)]
     fin_msgindex = trial_msgs[trial_msgs['text'].str.contains('termina experimento')].index[0]
     first_val = val_msgs.loc[:fin_msgindex]
     last_val  = val_msgs.loc[fin_msgindex:]
@@ -51,10 +51,33 @@ def save_validation_fixations(trial_msgs, trial_fix, trial_path):
     first_valfix = trial_fix[(trial_fix['tStart'] >= first_val.iloc[0]['time']) & (trial_fix['tEnd'] <= first_val.iloc[-1]['time'] + 300)]
     last_valfix  = trial_fix[(trial_fix['tStart'] >= last_val.iloc[0]['time']) & (trial_fix['tEnd'] <= last_val.iloc[-1]['time'] + 300)]
     
+    points_coords = val_msgs['text'].str.extract(r'(\d+),(\d+)').astype(int)[:num_points].to_numpy()
+    firstval_iscorrect = check_validation_fixations(first_valfix, points_coords, trial_path, num_points, points_area)
+    lastval_iscorrect  = check_validation_fixations(last_valfix, points_coords, trial_path, num_points, points_area)
+
+    if not firstval_iscorrect:
+        print('Validation error at the begining of trial for participant ', trial_path.parent.name, ' in trial ', trial_path.name)
+    if not lastval_iscorrect:
+        print('Validation error at the end of trial for participant ', trial_path.parent.name, ' in trial ', trial_path.name)
+        
     val_path = trial_path / 'validation'
     if not val_path.exists(): val_path.mkdir()
     first_valfix.to_pickle(val_path / 'first.pkl')
     last_valfix.to_pickle(val_path / 'last.pkl')
+    
+def check_validation_fixations(fixations, points_coords, num_points, points_area):
+    """ For each fixation, check if it is inside the area of the point.
+        As we only advance the point index once we find a fixation inside the area,
+        the validation is correct only if the point index matches the number of points - 1. """
+    fix_coords  = fixations.loc[:, ['xAvg', 'yAvg']].astype(int).to_numpy()
+    point_index = 0
+    for (x, y) in fix_coords:
+        lower_bound, upper_bound = points_coords[point_index] - points_area, points_coords[point_index] + points_area
+        if x in range(lower_bound[0], upper_bound[0]) and y in range(lower_bound[1], upper_bound[1]):
+            point_index += 1
+            if point_index == num_points - 1: break
+    
+    return point_index == num_points - 1
 
 def divide_data_by_screen(trial_sequence, trial_msgs, trial_fix, trial_sacc, trial_path, subj_name):
     for i, screen_id in enumerate(trial_sequence['currentscreenid']):
