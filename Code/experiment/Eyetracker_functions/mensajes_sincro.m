41f8aed1fbbd6cf96bb7e4d102e4dc9a10b93cfa
FerminT <fermintravi@gmail.com>
2023-07-14 19:21:26 -0300

    Cleanup!

--- Code/experiment/Eyetracker_functions/mensajes_sincro.m
+++
@@ -1,9 +0,0 @@
-
-% para poner mensajes de sincronizacion en el eyelink el comando es
-% "Eyelink('Message','mensaje'); Por ejemplo dentro de un loops de trial
-% apra decirle que me anote en el .edf cuando empezo un trial hay que poner
-% (adentro el loop):
-
-
-str=['trial(' num2str(i) ')'];
-Eyelink('Message', str);
