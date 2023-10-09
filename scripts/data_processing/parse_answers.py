5b677d6dca4a9ca48ec2f8e9dbd7e66aadc8efd9
FerminT <fermintravi@gmail.com>
2023-10-09 11:18:23 -0300

    Add some words mapping to words associations answers

--- scripts/data_processing/parse_answers.py
+++ scripts/data_processing/parse_answers.py
@@ -1,3 +1,14 @@
+WORDS_MAPPING = {
+    'anio': 'año',
+    'anos': 'años',
+    '21': 'veintiuno',
+    'xxi': 'veintiuno',
+    'pequenio': 'pequeño',
+    'seniora': 'señora',
+    'senior': 'señor',
+    'seniorita': 'señorita',
+    'maniana': 'mañana'
+}
 
 
 def parse_answer(answer):
@@ -6,4 +17,8 @@ def parse_answer(answer):
     else:
         answer = answer.lower()
         answer = answer.replace(';', 'ñ')
+        if len(answer.split()) > 1:
+            answer = answer.split()[-1]
+        if answer in WORDS_MAPPING:
+            answer = WORDS_MAPPING[answer]
     return answer
